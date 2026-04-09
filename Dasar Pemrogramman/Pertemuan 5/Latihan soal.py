5e81db88a0e6349c59c3c9822d1db40d03b41d4b
Arkensia859 <m.raihan.asyauqi@gmail.com>
2026-04-09 12:11:48 +0700

    Delete Dasar Pemrogramman directory

--- Dasar Pemrogramman/Pertemuan 5/Latihan soal.py
+++
@@ -1,8 +0,0 @@
-ulang = 2
-for i in range(ulang):
-    print("data ke-" + str(i+1))
-    nama= input("Masukkan Nim anda : ")
-    uts = int(input("Masukkan Nilai UTS anda : "))
-    uas = int(input("Masukkan Nilai UAS : "))
-    print("NIM anda adalah %s nilai UTS anda %i nilai UAS anda %i" % (nama, uts, uas))
-    print("-------------------------------------\n")
